191bf643740b4214eefcf0f37a9af84fa86068dd
lejlahadzovic <hadzoviclejla6@gmail.com>
2023-03-26 14:50:50 +0200

    Ispravka - smještaj url

--- Studentoglasi/Helper/Config.cs
+++ Studentoglasi/Helper/Config.cs
@@ -5,7 +5,10 @@ namespace StudentOglasi.Helper
     public class Config
     {
         public static string SlikePutanja = "https://localhost:7296/Slike/";
+        public static string CVPutanja = "https://localhost:7296/CV/";
         public static string Slike => "Slike/";
         public static string SlikeFolder => "wwwroot/" + Slike;
+        public static string CV => "CV/";
+        public static string CVFolder => "wwwroot/" + CV;
     }
 }
